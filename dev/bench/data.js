65b94098dc52aec900bb63294abad08e9d765d85
github-action-benchmark <github@users.noreply.github.com>
2022-08-31 20:47:49 +0000

    add TrustyAI continuous benchmarks (pytest) benchmark result for 731e3dd7eb154ef21b4ed484b1ecab553e5519ed

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661976008757,
+  "lastUpdate": 1661978869640,
   "repoUrl": "https://github.com/ruivieira/trustyai-explainability-python",
   "entries": {
     "TrustyAI continuous benchmarks": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0003463087515508682",
             "extra": "mean: 623.1016487277659 usec\nrounds: 1662"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruidevieira@googlemail.com",
+            "name": "Rui Vieira",
+            "username": "ruivieira"
+          },
+          "committer": {
+            "email": "ruidevieira@googlemail.com",
+            "name": "Rui Vieira",
+            "username": "ruivieira"
+          },
+          "distinct": true,
+          "id": "731e3dd7eb154ef21b4ed484b1ecab553e5519ed",
+          "message": "Restore saving to same repo",
+          "timestamp": "2022-08-31T21:43:51+01:00",
+          "tree_id": "0d130b8c4bae013a2df1c357f447d8e76c6112f9",
+          "url": "https://github.com/ruivieira/trustyai-explainability-python/commit/731e3dd7eb154ef21b4ed484b1ecab553e5519ed"
+        },
+        "date": 1661978867954,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/benchmark.py::test_counterfactual_match",
+            "value": 0.4302784115022133,
+            "unit": "iter/sec",
+            "range": "stddev: 0.20008620753224376",
+            "extra": "mean: 2.3240766286849976 sec\nrounds: 10"
+          },
+          {
+            "name": "tests/benchmarks/benchmark.py::test_non_empty_input",
+            "value": 3.6453889823539978,
+            "unit": "iter/sec",
+            "range": "stddev: 0.030994907904331223",
+            "extra": "mean: 274.31914806365967 msec\nrounds: 10"
+          },
+          {
+            "name": "tests/benchmarks/benchmark.py::test_counterfactual_match_python_model",
+            "value": 1.1697122484955826,
+            "unit": "iter/sec",
+            "range": "stddev: 0.017907188327231486",
+            "extra": "mean: 854.9111127853394 msec\nrounds: 10"
           }
         ]
       }
