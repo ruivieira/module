c25555eb9d8e2a20e3e89070453d384ca3133008
github-action-benchmark <github@users.noreply.github.com>
2022-08-31 13:09:59 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 1a92c36fc5b3aae7fb98d8133ed9b66299155718

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1661951399678,
+  "repoUrl": "https://github.com/ruivieira/trustyai-explainability-python",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruidevieira@googlemail.com",
+            "name": "Rui Vieira",
+            "username": "ruivieira"
+          },
+          "committer": {
+            "email": "ruidevieira@googlemail.com",
+            "name": "Rui Vieira",
+            "username": "ruivieira"
+          },
+          "distinct": true,
+          "id": "1a92c36fc5b3aae7fb98d8133ed9b66299155718",
+          "message": "Update benchmark",
+          "timestamp": "2022-08-31T14:07:10+01:00",
+          "tree_id": "41330d308357bed23ab109ff710689c84f644b62",
+          "url": "https://github.com/ruivieira/trustyai-explainability-python/commit/1a92c36fc5b3aae7fb98d8133ed9b66299155718"
+        },
+        "date": 1661951398099,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/benchmark.py::test_fib_10",
+            "value": 0.447788995457421,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06770123247467465",
+            "extra": "mean: 2.2331946746000084 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
